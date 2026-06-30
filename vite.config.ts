4bab606d0e5d723f827e6f488459eda4121b8a96
NicholasIliou <nicholas@iliou.de>
2026-06-30 18:18:16 +0200

    fix typeerror

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/marathon/"
+  base: "/marathon/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
